797b478b7d1d31fcd6c8a092b34778c9bbf620ff
Martynas Bardauskas <martynas.bardauskas@gmail.com>
2014-09-09 22:56:34 +0300

    added schema dump

---
+++ app/schema/schema.sql
@@ -0,0 +1,30 @@
+-- Adminer 4.1.0 MySQL dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+DROP TABLE IF EXISTS `silex_entry`;
+CREATE TABLE `silex_entry` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `datetime` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `subject` varchar(400) NOT NULL,
+  `text` longtext NOT NULL,
+  `owner_id` int(10) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `silex_user`;
+CREATE TABLE `silex_user` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `username` varchar(128) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `secret_key` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2014-09-09 19:55:09
